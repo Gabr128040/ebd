41fd3d888d8f6bb5b7ea4638f37726c1ad6f64d0
Gabr128040 <gabr.128040@gmail.com>
2023-02-28 16:34:36 -0300

    pasta template

---
+++ Template/Template.js
@@ -0,0 +1,10 @@
+function  menu() {
+	if (document.getElementsByClassName("nav")[0].style.display == 'none') {
+		document.getElementsByClassName("nav")[0].style.display = 'block'
+		document.getElementById("header-menu").innerText = 'close'
+	}
+	else {
+		document.getElementsByClassName("nav")[0].style.display = 'none'
+		document.getElementById("header-menu").innerText = 'menu'		
+	}
+}
